a54a355d53b8b4ea3a1eb0c5ac5319ea47fb51fb
Anshoool <anshuldu99@gmail.com>
2023-08-13 01:05:23 +0530

    main files

---
+++ src/handlers/linkspanel/topGG.js
@@ -0,0 +1,63 @@
+const Discord = require('discord.js');
+
+module.exports = async (client) => {
+    client.on(Discord.Events.InteractionCreate, async (interaction) => {
+        if (!interaction.isStringSelectMenu()) return;
+
+        if (interaction.customId == "Bot-linkspanel") {
+            if (interaction.values == "top.gg-linkspanel") {
+                interaction.deferUpdate();
+
+                const row2 = new Discord.ActionRowBuilder()
+                    .addComponents(
+                        new Discord.StringSelectMenuBuilder()
+                            .setCustomId('Bot-linkspanel')
+                            .setPlaceholder('❌┆Nothing selected')
+                            .addOptions([
+                                {
+                                    label: `Support server`,
+                                    description: `Join the suppport server`,
+                                    emoji: "❓",
+                                    value: "support-linkspanel",
+                                },
+                                {
+                                    label: `Invite Bot`,
+                                    description: `Invite Bot to your server`,
+                                    emoji: "📨",
+                                    value: "invite-linkspanel",
+                                },
+                                {
+                                    label: `Community Server`,
+                                    description: `Join the community server!`,
+                                    emoji: "🌍",
+                                    value: "community-linkspanel",
+                                },
+                                {
+                                    label: `Top.gg`,
+                                    description: `Show the top.gg link`,
+                                    emoji: "📃",
+                                    value: "top.gg-linkspanel",
+                                },
+                            ]),
+                    );
+
+                let row = new Discord.ActionRowBuilder()
+                    .addComponents(
+
+                        new Discord.ButtonBuilder()
+                            .setLabel("Vote Now")
+                            .setURL("https://google.com")
+                            .setStyle(Discord.ButtonStyle.Link),
+                    );
+
+                client.embed({
+                    title: `📃・Bot Vote`,
+                    desc: `Vote for Bot on top.gg`,
+                    components: [row2, row],
+                    type: 'edit'
+                }, interaction.message)
+            }
+        }
+    })
+}
+
